fbc086a47d453b0c66b1d5052167e996f8b7b33e
madsterest <odea.madeleine@gmail.com>
2021-06-03 21:28:13 +1000

    Created Model for Tasks

---
+++ models/Task.js
@@ -0,0 +1,36 @@
+const {Model, DataType, DataTypes} = require("sequelize"),
+const sequelize = require("../config/connection")
+
+class Task extends Model{};
+
+Task.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        project_title: {
+            type: DataTypes.VARCHAR(100),
+            allowNull: false,
+        },
+        project_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            references: {
+                model: 'project',
+                key: 'id'
+            }
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "task"
+
+    }
+    )
